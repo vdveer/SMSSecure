SamSprung/Signal-Android

1) created a new build flavor for espresso tests 2) create a new source set full of espresso tests 3) updated proguard-testing.pro 4) added test device numbers to .gitignore
// FREEBIE

test/androidTestEspresso/java/org/thoughtcrime/securesms/ApplicationPreferencesActivityActions.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.thoughtcrime.securesms;
+
+import org.hamcrest.Matchers;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
+
+public class ApplicationPreferencesActivityActions {
+
+  public static void clickSmsAndMmsSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_sms_mms"))).perform(click());
+  }
+
+  public static void clickNotificationsSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_notifications"))).perform(click());
+  }
+
+  public static void clickAppProtectionSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_app_protection"))).perform(click());
+  }
+
+  public static void clickAppearanceSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_appearance"))).perform(click());
+  }
+
+  public static void clickDeleteOldMessagesSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_storage"))).perform(click());
+  }
+
+  public static void clickAdvancedSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_advanced"))).perform(click());
+  }
+
+}

test/androidTestEspresso/java/org/thoughtcrime/securesms/ApplicationPreferencesActivityTest.java
@@ -1,0 +1,58 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.thoughtcrime.securesms;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.hamcrest.Matchers;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static org.thoughtcrime.securesms.EspressoUtil.waitOn;
+
+@LargeTest
+public class ApplicationPreferencesActivityTest extends TextSecureEspressoTestCase<ConversationListActivity> {
+
+  public ApplicationPreferencesActivityTest() {
+    super(ConversationListActivity.class);
+  }
+
+  private void checkAllPreferencesDisplayed() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_sms_mms")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_notifications")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_app_protection")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_appearance")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_storage")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_advanced")))
+          .check(matches(isDisplayed()));
+  }
+
+  public void testClickSettings() throws Exception {
+    loadActivity(ConversationListActivity.class, STATE_REGISTRATION_SKIPPED);
+    ConversationListActivityActions.clickSettings(getContext());
+    waitOn(ApplicationPreferencesActivity.class);
+    checkAllPreferencesDisplayed();
+  }
+
+}

test/androidTestEspresso/java/org/thoughtcrime/securesms/NewConversationActivityActions.java
@@ -1,0 +1,42 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.thoughtcrime.securesms;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+
+public class NewConversationActivityActions {
+
+  public static void filterNameOrNumber(String nameOrNumber) throws Exception {
+    EspressoUtil.replaceTextAndCloseKeyboard(onView(withId(R.id.filter)), nameOrNumber);
+  }
+
+  @SuppressWarnings("unchecked")
+  public static void clickContactWithName(String name) throws Exception {
+    onView(allOf(withId(R.id.name), withText(name))).perform(click());
+  }
+
+  @SuppressWarnings("unchecked")
+  public static void clickContactWithNumber(String number) throws Exception {
+    onView(allOf(withId(R.id.number), withText(containsString(number)))).perform(click());
+  }
+
+}

test/androidTestEspresso/java/org/thoughtcrime/securesms/ViewMatchers.java
@@ -1,0 +1,72 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.thoughtcrime.securesms;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class ViewMatchers {
+
+  public static Matcher<View> withRecyclerItem(final Matcher<Object> itemMatcher) {
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("with recycler item: ");
+        itemMatcher.describeTo(description);
+      }
+
+      @Override
+      public boolean matchesSafely(View view) {
+        if (!(view instanceof RecyclerView)) {
+          return false;
+        }
+
+        RecyclerView recyclerView = ((RecyclerView) view);
+        for (int i = 0; i < recyclerView.getChildCount(); i++) {
+          if (itemMatcher.matches(recyclerView.getChildAt(i))) {
+            return true;
+          }
+        }
+        return false;
+      }
+    };
+  }
+
+  public static Matcher<View> withRecyclerItemCount(final long itemCount) {
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("with recycler item count: " + itemCount);
+      }
+
+      @Override
+      public boolean matchesSafely(View view) {
+        if (!(view instanceof RecyclerView)) {
+          return false;
+        }
+
+        RecyclerView recyclerView = ((RecyclerView) view);
+        return recyclerView.getChildCount() == itemCount;
+      }
+    };
+  }
+
+}

test/androidTestEspresso/java/org/thoughtcrime/securesms/preferences/NotificationsPreferenceFragmentTest.java
@@ -1,0 +1,97 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.thoughtcrime.securesms.preferences;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.hamcrest.Matchers;
+import org.thoughtcrime.securesms.ApplicationPreferencesActivity;
+import org.thoughtcrime.securesms.ConversationListActivity;
+import org.thoughtcrime.securesms.ApplicationPreferencesActivityActions;
+import org.thoughtcrime.securesms.ConversationListActivityActions;
+import org.thoughtcrime.securesms.TextSecureEspressoTestCase;
+import org.thoughtcrime.securesms.util.TextSecurePreferences;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static org.thoughtcrime.securesms.EspressoUtil.waitOn;
+
+@LargeTest
+public class NotificationsPreferenceFragmentTest extends TextSecureEspressoTestCase<ConversationListActivity> {
+
+  public NotificationsPreferenceFragmentTest() {
+    super(ConversationListActivity.class);
+  }
+
+  private void checkAllPreferencesDisplayed() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("pref_key_enable_notifications")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_key_ringtone")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_key_vibrate")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_led_color")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_led_blink")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_key_inthread_notifications")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_repeat_alerts")))
+          .check(matches(isDisplayed()));
+  }
+
+  private void checkViewsMatchPreferences() throws Exception {
+    if (TextSecurePreferences.isNotificationsEnabled(getContext())) {
+      isChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_enable_notifications"))));
+    } else {
+      isNotChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_enable_notifications"))));
+    }
+
+    if (TextSecurePreferences.isNotificationVibrateEnabled(getContext())) {
+      isChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_vibrate"))));
+    } else {
+      isNotChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_vibrate"))));
+    }
+
+    if (TextSecurePreferences.isInThreadNotifications(getContext())) {
+      isChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_inthread_notifications"))));
+    } else {
+      isNotChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_inthread_notifications"))));
+    }
+  }
+
+  private void clickNotificationsSettingAndCheckState() throws Exception {
+    loadActivity(ConversationListActivity.class, STATE_REGISTRATION_SKIPPED);
+    ConversationListActivityActions.clickSettings(getContext());
+    waitOn(ApplicationPreferencesActivity.class);
+    ApplicationPreferencesActivityActions.clickNotificationsSetting();
+
+    checkAllPreferencesDisplayed();
+    checkViewsMatchPreferences();
+  }
+
+  public void testEnableNotifications() throws Exception {
+    clickNotificationsSettingAndCheckState();
+    NotificationsPreferenceFragmentActions.clickEnableNotifications();
+    isChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_enable_notifications"))));
+  }
+
+}

vdveer/SMSSecure

test/androidTestEspresso/java/org/smssecure/smssecure/ApplicationPreferencesActivityActions.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.smssecure.smssecure;
+
+import org.hamcrest.Matchers;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
+
+public class ApplicationPreferencesActivityActions {
+
+  public static void clickSmsAndMmsSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_sms_mms"))).perform(click());
+  }
+
+  public static void clickNotificationsSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_notifications"))).perform(click());
+  }
+
+  public static void clickAppProtectionSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_app_protection"))).perform(click());
+  }
+
+  public static void clickAppearanceSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_appearance"))).perform(click());
+  }
+
+  public static void clickDeleteOldMessagesSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_storage"))).perform(click());
+  }
+
+  public static void clickAdvancedSetting() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_advanced"))).perform(click());
+  }
+
+}

test/androidTestEspresso/java/org/smssecure/smssecure/ApplicationPreferencesActivityTest.java
@@ -1,0 +1,58 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.smssecure.smssecure;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.hamcrest.Matchers;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static org.smssecure.smssecure.EspressoUtil.waitOn;
+
+@LargeTest
+public class ApplicationPreferencesActivityTest extends TextSecureEspressoTestCase<ConversationListActivity> {
+
+  public ApplicationPreferencesActivityTest() {
+    super(ConversationListActivity.class);
+  }
+
+  private void checkAllPreferencesDisplayed() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("preference_category_sms_mms")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_notifications")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_app_protection")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_appearance")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_storage")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("preference_category_advanced")))
+          .check(matches(isDisplayed()));
+  }
+
+  public void testClickSettings() throws Exception {
+    loadActivity(ConversationListActivity.class, STATE_REGISTRATION_SKIPPED);
+    ConversationListActivityActions.clickSettings(getContext());
+    waitOn(ApplicationPreferencesActivity.class);
+    checkAllPreferencesDisplayed();
+  }
+
+}

test/androidTestEspresso/java/org/smssecure/smssecure/NewConversationActivityActions.java
@@ -1,0 +1,42 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.smssecure.smssecure;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+
+public class NewConversationActivityActions {
+
+  public static void filterNameOrNumber(String nameOrNumber) throws Exception {
+    EspressoUtil.replaceTextAndCloseKeyboard(onView(withId(R.id.filter)), nameOrNumber);
+  }
+
+  @SuppressWarnings("unchecked")
+  public static void clickContactWithName(String name) throws Exception {
+    onView(allOf(withId(R.id.name), withText(name))).perform(click());
+  }
+
+  @SuppressWarnings("unchecked")
+  public static void clickContactWithNumber(String number) throws Exception {
+    onView(allOf(withId(R.id.number), withText(containsString(number)))).perform(click());
+  }
+
+}

test/androidTestEspresso/java/org/smssecure/smssecure/ViewMatchers.java
@@ -1,0 +1,72 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.smssecure.smssecure;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class ViewMatchers {
+
+  public static Matcher<View> withRecyclerItem(final Matcher<Object> itemMatcher) {
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("with recycler item: ");
+        itemMatcher.describeTo(description);
+      }
+
+      @Override
+      public boolean matchesSafely(View view) {
+        if (!(view instanceof RecyclerView)) {
+          return false;
+        }
+
+        RecyclerView recyclerView = ((RecyclerView) view);
+        for (int i = 0; i < recyclerView.getChildCount(); i++) {
+          if (itemMatcher.matches(recyclerView.getChildAt(i))) {
+            return true;
+          }
+        }
+        return false;
+      }
+    };
+  }
+
+  public static Matcher<View> withRecyclerItemCount(final long itemCount) {
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("with recycler item count: " + itemCount);
+      }
+
+      @Override
+      public boolean matchesSafely(View view) {
+        if (!(view instanceof RecyclerView)) {
+          return false;
+        }
+
+        RecyclerView recyclerView = ((RecyclerView) view);
+        return recyclerView.getChildCount() == itemCount;
+      }
+    };
+  }
+
+}

test/androidTestEspresso/java/org/smssecure/smssecure/preferences/NotificationsPreferenceFragmentTest.java
@@ -1,0 +1,97 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.smssecure.smssecure.preferences;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.hamcrest.Matchers;
+import org.smssecure.smssecure.ApplicationPreferencesActivity;
+import org.smssecure.smssecure.ConversationListActivity;
+import org.smssecure.smssecure.ApplicationPreferencesActivityActions;
+import org.smssecure.smssecure.ConversationListActivityActions;
+import org.smssecure.smssecure.TextSecureEspressoTestCase;
+import org.smssecure.smssecure.util.SMSSecurePreferences;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static org.smssecure.smssecure.EspressoUtil.waitOn;
+
+@LargeTest
+public class NotificationsPreferenceFragmentTest extends TextSecureEspressoTestCase<ConversationListActivity> {
+
+  public NotificationsPreferenceFragmentTest() {
+    super(ConversationListActivity.class);
+  }
+
+  private void checkAllPreferencesDisplayed() throws Exception {
+    onData(Matchers.<Object>allOf(withKey("pref_key_enable_notifications")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_key_ringtone")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_key_vibrate")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_led_color")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_led_blink")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_key_inthread_notifications")))
+          .check(matches(isDisplayed()));
+    onData(Matchers.<Object>allOf(withKey("pref_repeat_alerts")))
+          .check(matches(isDisplayed()));
+  }
+
+  private void checkViewsMatchPreferences() throws Exception {
+    if (SMSSecurePreferences.isNotificationsEnabled(getContext())) {
+      isChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_enable_notifications"))));
+    } else {
+      isNotChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_enable_notifications"))));
+    }
+
+    if (SMSSecurePreferences.isNotificationVibrateEnabled(getContext())) {
+      isChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_vibrate"))));
+    } else {
+      isNotChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_vibrate"))));
+    }
+
+    if (SMSSecurePreferences.isInThreadNotifications(getContext())) {
+      isChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_inthread_notifications"))));
+    } else {
+      isNotChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_inthread_notifications"))));
+    }
+  }
+
+  private void clickNotificationsSettingAndCheckState() throws Exception {
+    loadActivity(ConversationListActivity.class, STATE_REGISTRATION_SKIPPED);
+    ConversationListActivityActions.clickSettings(getContext());
+    waitOn(ApplicationPreferencesActivity.class);
+    ApplicationPreferencesActivityActions.clickNotificationsSetting();
+
+    checkAllPreferencesDisplayed();
+    checkViewsMatchPreferences();
+  }
+
+  public void testEnableNotifications() throws Exception {
+    clickNotificationsSettingAndCheckState();
+    NotificationsPreferenceFragmentActions.clickEnableNotifications();
+    isChecked().matches(onData(Matchers.<Object>allOf(withKey("pref_key_enable_notifications"))));
+  }
+
+}