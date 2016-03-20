b37dc5eae5801db760298dadabf3632f35243ece
vdveer <wouter.van.der.veer@topicus.nl>
2016-03-20 22:14:30 +0100

    squashed mms size options

--- src/org/smssecure/smssecure/jobs/MmsSendJob.java
+++ src/org/smssecure/smssecure/jobs/MmsSendJob.java
@@ -26,6 +26,7 @@ import org.smssecure.smssecure.transport.InsecureFallbackApprovalException;
 import org.smssecure.smssecure.transport.UndeliverableMessageException;
 import org.smssecure.smssecure.util.Hex;
 import org.smssecure.smssecure.util.NumberUtil;
+import org.smssecure.smssecure.util.SMSSecurePreferences;
 import org.smssecure.smssecure.util.SmilUtil;
 import org.smssecure.smssecure.util.TelephonyUtil;
 import org.whispersystems.jobqueue.JobParameters;
@@ -107,6 +108,7 @@ public class MmsSendJob extends SendJob {
   private byte[] getPduBytes(MasterSecret masterSecret, SendReq message)
       throws IOException, UndeliverableMessageException, InsecureFallbackApprovalException
   {
+
     String number = TelephonyUtil.getManager(context).getLine1Number();
 
     message = getResolvedMessage(masterSecret, message, MediaConstraints.MMS_CONSTRAINTS, true);
